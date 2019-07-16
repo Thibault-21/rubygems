5f8eccb898e531b9f52c95c94627af55e202971d
Thibault-21 <43401546+Thibault-21@users.noreply.github.com>
2019-07-16 16:42:03 +0200

    update

--- lib/multiple.rb
+++ lib/multiple.rb
@@ -1,11 +1,14 @@
 def multiple(n) #is_multiple_of_3_or_5(n)
-if n == 3 or 5 
-  return true 
-elsif n != 3 or 5 
-  return false
-end 
-def sum(final_number) #sum_of_3_or_5_multiples(final_number)
-  (1..1000) 
-if n == true 
-  return final_number.sum
+  if n == 3 or 5 
+    return true 
+  elsif n != 3 or 5 
+    return false
   end
+end 
+
+def sum(final_number) #sum_of_3_or_5_multiples(final_number)
+  # (1..1000) 
+  if n == true 
+    return final_number.sum
+  end
+end
--- spec/multiples_spec.rb
+++ spec/multiples_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../projets_0/lib/multiple.rb'
+require_relative '../lib/multiple'
 
 describe "the is_multiple_of_3_or_5? method" do
   it "should return TRUE when an integer is a multiple of 3 or 5" do
@@ -9,11 +9,11 @@ describe "the is_multiple_of_3_or_5? method" do
   end
   
   it "should return FALSE when an integer is NOT a multiple of 3 or 5" do
-    expect(is_multiple_of_3_or_5?)(2)).to eq(false)
-    expect(is_multiple_of_3_or_5?)(7)).to eq(false)
-    expect(is_multiple_of_3_or_5?)(11)).to eq(false)
+    expect(is_multiple_of_3_or_5?(2)).to eq(false)
+    expect(is_multiple_of_3_or_5?(7)).to eq(false)
+    expect(is_multiple_of_3_or_5?(11)).to eq(false)
   end
 end
 
 # describe "the sum_of_3_or_5_multiples method" do 
-#   it "should "
+#   it "should return "
